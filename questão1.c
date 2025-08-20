822af4e78ee41f8052a94f06e5aecb74af619618
Arthuroa22 <arthuroliveiraaraujo@alunos.utfpr.edu.br>
2025-08-20 11:40:40 -0300

    Add files via upload

---
+++ questão1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+	float n1, n2, aux;
+
+	printf("Digite o primeiro valor: ");
+	scanf("%f", &n1);
+	printf("digite o segundo valor: ");
+	scanf("%f", &n2);
+
+	aux= n1 + n2;
+	
+	printf ("resultado %.2f\n", aux);
+	
+	return 0;
+	
+
+
+
+
+
+}
