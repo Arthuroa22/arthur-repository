8316bee6597aead22bde35aa6109b5adadb7f2f7
Arthuroa22 <arthuroliveiraaraujo@alunos.utfpr.edu.br>
2025-10-08 09:29:05 -0300

    Add files via upload

---
+++ ex1funcoes2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+void converteHora(int totalSegundos, int *hora, int *min, int *seg);
+
+int main() {
+	int totalSegundos;
+	int hora, min, seg;
+
+
+	printf("Digite o total de segundos: ");
+	scanf("%d", &totalSegundos);
+
+
+	converteHora(totalSegundos, &hora, &min, &seg);
+
+	printf("Resultado: %02d:%02d:%02d\n", hora, min, seg);
+
+	return 0;
+}
+
+void converteHora(int totalSegundos, int *hora, int *min, int *seg) {
+	*hora = totalSegundos / 3600;
+	totalSegundos = totalSegundos % 3600;
+	*min = totalSegundos / 60;
+	*seg = totalSegundos % 60;
+}
